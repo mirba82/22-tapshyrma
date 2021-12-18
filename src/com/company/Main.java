9354589a033fe4cee1be6484935ecaa98ae5a26a
79254 <mirbek.abylov.kk@gmail.com>
2021-12-19 01:43:12 +0300

    haha1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,29 +1,20 @@
 package com.company;
 
-import java.io.IOException;
 
-public class Main {/*public static void main(String[] args) {
-    hereWillBeTrouble(42, 0);
-}
 
-private static void hereWillBeTrouble(int a, int b) {
-    if (b == 0) {
-        throw new ArithmeticException("ты опять делишь на ноль?");
-    }
-    int oops = a / b;
-    System.out.println(oops);
-}
-*/
+public class Main {
+
     /*Try with resources-те колдоно ала турган Car деген класс тузунуз
 close() деген метод "Машина жабылып жатат" деп консольго чыгаруусу керек
-try unchecked exceptionдарды кармап аларды игнор кылыш керек
-main методдо try блоктун ичинде drive() деген методду чакырыныз, ал консольго "Машина журуп жатат" деп чыгаруусу керек
+try unchecked exception дарды кармап аларды игнор кылыш керек
+main методдо try блоктун ичинде drive() деген методду чакырыныз,
+ал консольго "Машина журуп жатат" деп чыгаруусу керек
 
 Эскертуу: маселени чыгарууда try with resources колдонуу керек*/
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         // write your code here
-try( Car car = new Car();) {
+try( Car car = new Car()) {
 
     car.drive();
 }catch(Exception e ){
